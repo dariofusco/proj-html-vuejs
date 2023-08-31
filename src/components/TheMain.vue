e1f5a987f1b73e3edc6bfd2a061ca01b30a9a7a8
dariofusco <fdario@hotmail.it>
2023-08-31 15:10:39 +0200

    inserisce hero

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -5,9 +5,23 @@ export default {
 </script>
 
 <template>
-<main></main>
+<main>
+    <div class="hero text-center">
+        <h3>Commence Business</h3>
+        <h1>Distant Mentoring Program</h1>
+        <button type="button" class="btn btn-light">Download free guidebook <i class="fa-solid fa-arrow-right-long"></i></button>
+    </div>
+</main>
 </template>
 
 <style lang="scss" scoped>
-
+.hero {
+    background-image: url(../assets/img/home-3-hero-bg.jpg);
+    height: 700px;
+    padding: 250px;
+    color: white;
+}
+.btn {
+    color: #20ad96;
+}
 </style>
